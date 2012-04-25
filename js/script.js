0fc06fbbf2df54a0550b595e1d94385f6866bb3c
Wylan Werth <wylan.werth@gmail.com>
2012-04-25 12:47:49 -0600

    Rewrote the click function. local storage now stores the state for each quiz. So that it can be displayed when the user next returns.
    Signed-off-by: Wylan Werth <wylan.werth@gmail.com>

--- js/script.js
+++ js/script.js
@@ -8,45 +8,71 @@ $("#quizone").live("pageshow", function (event) {
       var scoreString = "Score: " + score;
       $("#score").text(scoreString);  
 });
-
-var $page;
+var currentPage;
+function quizCompleted(page) {
+  // check to see if this question has been answered, if so we need to disable it
+  if (localStorage.getItem(page) == "true") {
+    //console.log(page + " returning true");
+    return true;
+  }else {
+    //console.log(page + " returning false");
+    return false;
+  };
+}
 
 $(".quizpage").live("pageshow", function (event) {
-      $page = $.mobile.activePage;
       //console.log($page.attr("id"));
+      currentPage = $(this).attr("id");
+      if (quizCompleted(currentPage) == true) {
+        $("div.ui-page-active h1.banner.right").removeClass("hide");
+        $("div.ui-page-active a.right").css('background-color', 'green');
+      };
 });
 
 $( document ).delegate(".quizpage", "pageinit", function() {
+  // grab the current score
   var score = parseInt(localStorage.getItem("score"));
-  //alert('A page with a class of "quizpage" was just created by jQuery Mobile!');
-  $('a.wrong').click(function(e){
-        // change the background color of the clicked link (update needed to keep corners round)
-         $(this).parent().css('background-color', 'red');
-
+  if (quizCompleted($(this).attr("id")) == true) {
+    $("div.ui-page-active h1.banner.right").removeClass("hide");
+    $("div.ui-page-active a.right").css('background-color', 'green');
+  };
+  
+  $('a.answer').click(function(e){
+         // disable the buttons because this quiz has been completed
+         if (quizCompleted(currentPage) == true) {
+           e.preventDefault();
+           return;
+         }else {
+           
+         };
          // create selectors for the page banners
          var bannerWrong = $("div.ui-page-active h1.banner.wrong");
          var bannerRight = $("div.ui-page-active h1.banner.right");
-         // hide the correct banner and display the incorrect banner
-         bannerWrong.removeClass("hide");
-         bannerRight.addClass("hide");
+         
+         if ($(this).hasClass("wrong")) {
+           // change the background color of the clicked link (update needed to keep corners round)
+            $(this).parent().css('background-color', 'red');
+            // hide the correct banner and display the incorrect banner
+            bannerWrong.removeClass("hide");
+            bannerRight.addClass("hide");
+         }else if($(this).hasClass("right")) {
+           // change the background color of the clicked link (update needed to keep corners round)
+            $(this).parent().css('background-color', 'green');
+           // hide the correct banner and display the incorrect banner
+            bannerRight.removeClass("hide");
+            bannerWrong.addClass("hide");
+            
+            // update the score in the localStorage database
+            score += 10;
+            localStorage.setItem("score", score);
+            
+            // store that this quiz has been answered sucessfully
+            //console.log("setting " +currentPage + " true");
+            localStorage.setItem(currentPage, true);
+         };
     
 
   });
   
-$('a.right').click(function(e){
-     // change the background color of the clicked link (update needed to keep corners round)
-      $(this).parent().css('background-color', 'green');
-
-      // create selectors for the page banners
-      var bannerWrong = $("div.ui-page-active h1.banner.wrong");
-      var bannerRight = $("div.ui-page-active h1.banner.right");
-      // hide the correct banner and display the incorrect banner
-      bannerRight.removeClass("hide");
-      bannerWrong.addClass("hide");
-
-      // update the score in the localStorage database
-      score += 10;
-      localStorage.setItem("score", score);
-    });
 });
 
